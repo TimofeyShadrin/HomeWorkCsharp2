159be539440db5bfa084be7a24c98d3f7b4eef5a
Timofei Shadrin <tshadrin@yandex.ru>
2022-06-28 18:17:36 +0300

    Task 13 added

---
+++ Task13/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace HelloWorld 
+{ class Program 
+    { static void Main(string[] args) 
+        { 
+            Console.Write("Enter a three-digit number: ");
+            string? number = Console.ReadLine();
+            if (!string.IsNullOrEmpty(number))
+            {
+                if (number.Length >= 3)
+                {
+                    Console.WriteLine($"Your third digit is: {number[2]}");
+                }
+                else
+                {
+                    Console.WriteLine($"There is no third digit!");
+                }
+            }
+        } 
+    } 
+} 
