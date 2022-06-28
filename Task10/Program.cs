be37439e8132ca21d6b4637d776a27310d54076e
Timofei Shadrin <tshadrin@yandex.ru>
2022-06-28 18:07:21 +0300

    Task10 added

---
+++ Task10/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace HelloWorld 
+{ class Program 
+    { static void Main(string[] args) 
+        { 
+            Console.Write("Enter a three-digit number: ");
+            string? number = Console.ReadLine();
+            if (!string.IsNullOrEmpty(number))
+            {
+                Console.WriteLine($"Your second digit is: {number[1]}");
+            }
+        } 
+    } 
+} 
