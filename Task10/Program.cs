c7c7962a4165cd56e83108d1680066b5e8a2024d
Timofei Shadrin <tshadrin@yandex.ru>
2022-06-29 17:32:52 +0300

    Task conditions added in Task 10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,4 +1,11 @@
-﻿namespace HelloWorld 
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и 
+// на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+namespace HelloWorld 
 { class Program 
     { static void Main(string[] args) 
         { 
