5390573bf39354d918e698767226d03531773cec
Timofei Shadrin <tshadrin@yandex.ru>
2022-06-30 19:26:38 +0300

    Autoformat Ctrl+Shift+I used

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -5,44 +5,48 @@
 // 7 -> да
 // 1 -> нет
 
-namespace HelloWorld 
-{ class Program 
-    { static void Main(string[] args) 
-        { 
-            while (true) {
-            try {
-            Console.Clear(); 
-            Console.Write("Enter the number of the day of the week: ");
-            string? digit = Console.ReadLine();
-            Console.WriteLine();
-            
-            if (!string.IsNullOrEmpty(digit)) 
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
             {
-                char number = Convert.ToChar(digit.ToString());
-            if (number == '6' || number == '7')
+                try
                 {
-                    Console.WriteLine("This day is a day off");
+                    Console.Clear();
+                    Console.Write("Enter the number of the day of the week: ");
+                    string? digit = Console.ReadLine();
                     Console.WriteLine();
-                    break;
+
+                    if (!string.IsNullOrEmpty(digit))
+                    {
+                        char number = Convert.ToChar(digit.ToString());
+                        if (number == '6' || number == '7')
+                        {
+                            Console.WriteLine("This day is a day off");
+                            Console.WriteLine();
+                            break;
+                        }
+                        else if (number == '1' || number == '2' || number == '3' || number == '4' || number == '5')
+                        {
+                            Console.WriteLine("This day is a working day");
+                            Console.WriteLine();
+                            break;
+                        }
+                        else
+                        {
+                            continue;
+                        }
+                    }
+
                 }
-            else if (number == '1' || number == '2' || number == '3' || number == '4' || number == '5')
-                {
-                    Console.WriteLine("This day is a working day");
-                    Console.WriteLine();
-                    break;
-                }
-            else
+                catch
                 {
                     continue;
                 }
             }
-            
-            }
-            catch
-            {
-                continue;
-            }
-            }
-        } 
-    } 
-} 
+        }
+    }
+}
